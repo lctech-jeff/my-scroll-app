15a2a0de2e7092289a893cae9621c50fa4ca4dd1
lctech-jeff <jeff@lctech.com.tw>
2024-11-15 15:34:27 +0800

    ci: add gh-pages

--- src/composable/useCPU.ts
+++ src/composable/useCPU.ts
@@ -1,23 +1,27 @@
 const state = ref<string>('')
 
-const callback = (records: unknown[]) => {
-  const lastRecord = records[records.length - 1];
+const callback = (records: unknown[]): void => {
+  const lastRecord = records[records.length - 1]
   // @ts-expect-error
   state.value = lastRecord.state
 }
 
-try {
-  // @ts-expect-error
-  const observer = new PressureObserver(callback);
-  await observer.observe("cpu", {
-    sampleInterval: 1000, // 1000ms
-  });
-} catch (e: unknown) {
-  console.log(e)
+const observeCPU = async (): Promise<void> => {
+  try {
+    // @ts-expect-error
+    const observer = new PressureObserver(callback)
+    await observer.observe('cpu', {
+      sampleInterval: 1000, // 1000ms
+    })
+  } catch (e: unknown) {
+    console.log(e)
+  }
 }
+
+observeCPU()
 
 export const useCPU = () => {
   return {
-    state
+    state,
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/my-scroll-app/",
   plugins: [
     vue(),
     AutoImport({
